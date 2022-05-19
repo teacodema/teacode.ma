790b0556d5a8bca83ebffda6691e751d9d6d6aaa
Driss <drissboumlik.dev@gmail.com>
2022-05-19 19:53:21 +0100

    Clear unused action (commented)

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -35,21 +35,6 @@ class PageController extends Controller
         return view('pages.contributors', ['data' => $data]);
     }
 
-    // public function interview(Request $request)
-    // {
-    //     $data = new \stdClass;
-    //     $data->title = 'TeaCode | Book Interview';
-    //     $data->availabilities = json_decode(\File::get(base_path() . '/database/data/interview-availability.json'));
-    //     return view('pages.book-interview', ['data' => $data]);
-    // }
-
-    // public function calendar(Request $request)
-    // {
-    //     $data = new \stdClass;
-    //     $data->title = 'TeaCode | Calendar';
-    //     return view('pages.calendar', ['data' => $data]);
-    // }
-
     public function resources(Request $request, $page)
     {
         $data = new \stdClass;
@@ -71,21 +56,6 @@ class PageController extends Controller
         $data->title = 'TeaCode | Terms of Use';
         return view('pages.terms-of-use', ['data' => $data]);
     }
-
-    // public function comingSoon(Request $request)
-    // {
-    //     $data = new \stdClass;
-    //     $data->activities = json_decode(\File::get(base_path() . '/database/data/activities.json'));
-    //     $data->title = 'TeaCode | Coming Soon ...';
-    //     return view('pages.coming-soon', ['data' => $data]);
-    // }
-
-    // public function feedback(Request $request)
-    // {
-    //     $data = new \stdClass;
-    //     $data->title = 'TeaCode | Feedback';
-    //     return view('pages.feedback');
-    // }
 
     // public function rules(Request $request)
     // {
